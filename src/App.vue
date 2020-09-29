2ae053b99803234edae4577fa50fd439af6314c1
blacktiny <blacktiny126@hotmail.com>
2020-09-28 19:33:32 -0700

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,30 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <MainFrame />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainFrame from './components/MainFrame.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    MainFrame
   }
 }
 </script>
 
-<style>
+<style lang="scss">
+@import '@/assets/styles/globalStyles.scss';
+
+body {
+  margin: 0;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
