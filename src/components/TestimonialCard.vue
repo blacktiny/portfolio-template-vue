29510158444591b5e68986d4d0a475c368e37b8b
blacktiny <blacktiny126@hotmail.com>
2020-09-30 12:54:26 -0700

    update about

---
+++ src/components/TestimonialCard.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="content row">
+    <div class="photo-container">
+      <img :src="require(`@/assets/images/${data.imgURL}`)" />
+    </div>
+    <div class="column card-box">
+      <div class="triangle-left" />
+      <p class="quote">
+        "{{ data.quote }}"
+      </p>
+      <div class="quote-author">
+        <span>- {{ data.author }},</span>&nbsp;
+        <span>{{ data.role }}</span>&nbsp;
+        <span class="quote-company">{{ data.company }}</span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TestimonialCard',
+  props: ['data'],
+  mounted () {
+  }
+}
+</script>
+
+<style lang="scss">
+
+.content {
+  width: 50%;
+
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+}
+
+.photo-container {
+  flex: 0 0 50px;
+}
+
+.photo-container img {
+  width: 100%;
+  border-radius: 50%;
+  box-shadow: 0 2px 10px 0px rgba(0, 0, 0, 0.2);
+}
+
+.card-box {
+  position: relative;
+  background: #222222;
+  border-radius: 7px;
+  box-shadow: 0 2px 30px -2px rgba(0, 0, 0, 0.1);
+  padding: 30px;
+  margin: 0 0 1.5rem 2.5rem;
+}
+
+.triangle-left {
+  position: absolute;
+  top: 30px;
+  left: -20px;
+  width: 0;
+  height: 0;
+  border-bottom: 20px solid transparent;
+  border-right: 20px solid #222;
+}
+
+.quote {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+  color: rgba(255, 255, 255, 0.4);
+  line-height: 1.7;
+  font-size: 16px;
+  font-weight: 300;
+  text-align: center;
+  padding: 0;
+  margin: 0 0 1rem 0;
+}
+
+.quote-author {
+  font-style: italic;
+  font-size: 14px;
+  line-height: 1.8;
+  color: #666666;
+}
+
+.quote-company {
+  color: #bac964;
+}
+
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="column container about">
+    <div class="row about-me">
+      <div class="photo">
+        
+      </div>
+      <div class="column right-box">
+        <p class="header-text">
+          <span>About</span>
+          <span>Me</span>
+        </p>
+        <div class="summary">
+          {{ data.summary }}
+        </div>
+      </div>
+    </div>
+    <div class="testimonial">
+      <div class="section-heading">
+        <h2>Client <strong>Testimonial</strong></h2>
+      </div>
+      <TestimonialCard v-for="item in data.testimonials" :key="item.id" :data="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import TestimonialCard from '@/components/TestimonialCard.vue'
+
+export default {
+  name: 'About',
+  components: {
+    TestimonialCard
+  },
+  data: () => ({
+    data: {
+      summary: `Passionate front-end software developer with 6 years of experience using HTML5/CSS3, Bootstrap, jQuery and Javascript/Typescript to build all aspects of the UX and UI for many kinds of web sites. Specialized in using ReactJS, VueJS, Angular and PHP/Laravel to build many kinds of web platforms and mobile apps.`,
+      testimonials: [
+        {
+          id: 't-1',
+          imgURL: 'quote_author-1.jpeg',
+          quote: `First time we worked together, Mark came up with several on-target creative concepts under significant deadline pressure. he's creative, deliberate in his strategy and focused on results. he knows that online content is king and understands how to pull leads into the pipeline.`,
+          author: 'David Lung',
+          role: 'CEO',
+          company: 'GoGoGrandparent Inc.',
+        }
+      ]
+    }
+  })
+}
+</script>
+
+<style lang="scss">
+
+.about {
+  position: relative;
+}
+
+.about-me {
+  margin-top: 200px;
+}
+
+.photo {
+  width: 40%;
+  height: 300px;
+  border: 2px solid #323232;
+  margin-right: 10%;
+}
+
+.right-box {
+  width: 50%;
+  text-align: left;
+
+  .header-text {
+    & > span:nth-child(1) {
+      font-size: 2em;
+      margin-right: 10px;
+    }
+    & > span:nth-child(2) {
+      font-size: 2em;
+      font-weight: bold;
+    }
+  }
+
+  .summary {
+    font-size: 1.2em;
+    color: rgba(255, 255, 255, 0.5);
+  }
+}
+
+.testimonial {
+  margin-top: 200px;
+}
+
+</style>
